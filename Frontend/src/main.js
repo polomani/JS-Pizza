1390d3804515c8ba1b5fadc60cd7d3bce92c5ad1
Ivano Polomani <ivano.polomani@gmail.com>
2016-11-14 17:24:45 +0200

    validation on order page
    *logo fixed
    *btn edit-order added

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -11,5 +11,49 @@ $(function(){
     PizzaCart.initialiseCart();
     PizzaMenu.initialiseMenu();
 
+    jQuery.validator.addMethod("onlyletters", 
+        function(value, element) {
+            return /^[a-zA-ZА-Яа-яієїІЄЇ]+$/.test(value);
+        }, "");
+
+    jQuery.validator.addMethod("phone", 
+        function(value, element) {
+            return /^(\+?(380[0-9]{9})|(0[0-9]{9}))+$/.test(value);
+        }, "");
+    
+    $("#submit").click(function() {
+        $("#buyform").valid();    
+    });
+    
+    $("#buyform").validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+          onlyletters: true
+        },
+        phone: {
+          required: true,
+          phone: true
+        },
+        address: {
+          required: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Будь ласка введіть тільки ваше ім'я, без цифр",
+          minlength: "Будь ласка введіть тільки ваше ім'я, без цифр",
+          onlyletters: "Будь ласка введіть тільки ваше ім'я, без цифр"
+        },
+        phone: {
+          required: "Введіть номер телефону у форматі +380 або почніть з 0",
+          phone: "Введіть номер телефону у форматі +380 або почніть з 0"
+        },
+        address: {
+          required:"Введіть адресу доставки піци"
+        }
+      }
+    });
 
 });
